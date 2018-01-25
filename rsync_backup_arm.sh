72c3dfd11451b30e3aaa60e2685c7f42189a2fcc
Joel Hutton <joel.hutton@arm.com>
2018-01-25 11:51:46 +0000

    Added rsync incremental backup script

---
+++ rsync_backup_arm.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+DATE=`date "+%Y-%m-%dT%H:%M:%S"`
+BACKUP_NAME="$DATE\_full_fs"
+BACKUP_PATH=/data/backups/rsync
+HOST=joehut01@e115011-lin.cambridge.arm.com
+
+#x - don't cross file system boundaries
+#a - archive (preserve permissions, symlinks etc.)
+#r - recursive
+#P - partial, pickup partial copies
+#z - use zip compression to speedup transfer
+sudo rsync -arvzPx \
+	--exclude /home/joehut01/Downloads \
+	--exclude /tmp --exclude /mnt \
+	--exclude /media --exclude /proc \
+	--exclude /arm --exclude /run \
+	--exclude /dev --exclude /home/joehut01/.cache \
+	--exclude /sys \
+	/ \
+	--link-dest=$BACKUP_PATH/current \
+	$HOST:$BACKUP_PATH/$BACKUP_NAME &&\
+ssh $HOST  "rm -f $BACKUP_PATH/current \
+	&& ln -s $BACKUP_PATH/$BACKUP_NAME $BACKUP_PATH/current"
