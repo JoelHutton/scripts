fcc68de1c1e2874bfc49754499b58a807254c6fb
Joel Hutton <joel.hutton@arm.com>
2018-02-19 16:49:00 +0000

    update_copyright.sh: Added check for "ARM"
    Check in regex substitution, that substituted line includes "ARM"
    to prevent mangling someone elses copyright.

--- update_copyright.sh
+++ update_copyright.sh
@@ -11,10 +11,10 @@ while read -r FILE; do
 		if grep "Copyright" "$FILE" >/dev/null
 		then
 			#change year range to startyear-current year
-			sed -i "s/\(Copyright[^0-9]*[0-9][0-9][0-9][0-9]-\)\([0-9][0-9][0-9][0-9]\)/\1$YEAR/" $FILE
+			sed -i "s/\(Copyright[^0-9]*[0-9][0-9][0-9][0-9]-\)\([0-9][0-9][0-9][0-9]\).*\(ARM\|Arm\)/\1$YEAR/" $FILE
 
 			#change single year to year-current year
-			sed -i "s/\(Copyright[^0-9]*[0-9][0-9][0-9][0-9]\)\([^\-]\)/\1-$YEAR\2/" $FILE
+			sed -i "s/\(Copyright[^0-9]*[0-9][0-9][0-9][0-9]\)\([^\-]\).*\(ARM\|Arm\)/\1-$YEAR\2/" $FILE
 			git add "$FILE"
 		else
 			echo "No copyright in file"
