8c982f723ffca4fff7a0ec198b97707f9e2e1801
Joel Hutton <Joel.Hutton@Arm.com>
2018-05-15 11:11:30 +0100

    Tidied up update_copyright

--- update_copyright.sh
+++ update_copyright.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
 FILES=`git diff HEAD  --name-only`
-
 YEAR=`date +"%Y"`
+
+YEAR_RGX="[0-9][0-9][0-9][0-9]"
+ARM_RGX="\(ARM\|Arm\|arm\)"
+
 while read -r FILE; do
 	if [ -z "$FILE" ]
 	then
 		break
 	fi
-	if grep "opyright.*$YEAR.*\(ARM\|Arm\|arm\)" "$FILE">/dev/null
+	if grep "opyright.*$YEAR.*$ARM_RGX" "$FILE">/dev/null
 	then
 		echo "$FILE:File already correctly copyrighted"
 	else
-		if grep "opyright.*\(ARM\|Arm\|arm\)" "$FILE" >/dev/null
+		if grep "opyright.*$ARM_RGX" "$FILE" >/dev/null
 		then
 			#change year range to startyear-current year
-			sed -i "s/\(opyright[^0-9]*[0-9][0-9][0-9][0-9]-\)\([0-9][0-9][0-9][0-9]\)\(.*ARM\|.*Arm\|.*arm\)/\1$YEAR\3/" $FILE
+			sed -i "s/\(opyright.*$YEAR_RGX-\)\($YEAR_RGX\)\(.*$ARM_RGX\)/\1$YEAR\3/" $FILE
 
 			#change single year to year-current year
-			sed -i "s/\(opyright[^0-9]*[0-9][0-9][0-9][0-9]\)\([^\-]\)\(.*ARM\|.*Arm\|.*arm\)/\1-$YEAR\2\3/" $FILE
+			sed -i "s/\(opyright.*[^\-]$YEAR_RGX\)\([^\-]\)\(.*$ARM_RGX\)/\1-$YEAR\2\3/" $FILE
 			echo "Changed copyright year of: $FILE"
 			git add "$FILE"
 		else
