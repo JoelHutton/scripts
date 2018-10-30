553a70af5c01bf5e8c4b8a1c8bb07d3b6e2a2d9f
Joel Hutton <Joel.Hutton@Arm.com>
2018-10-30 16:24:48 +0000

    toggle_layout:make alt_l same as alt_r
    Change-Id: I3a57bf6676df0557f8d3a34030d6b7f32ef450ba

--- toggle_layout.sh
+++ toggle_layout.sh
@@ -4,6 +4,8 @@ CUR_LAYOUT=`setxkbmap -query`
 if [[ -z `echo $CUR_LAYOUT | grep 'dvorak'` ]]
 then
 	setxkbmap -layout "gb" -variant "dvorak" -option caps:swapescape
+	xmodmap -e "clear mod5"
+	xmodmap -e "keycode 108 = Alt_L"
 else
 	setxkbmap -layout "gb"
 fi
