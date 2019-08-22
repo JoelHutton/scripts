ffe2205e2a5f6ddde9070314e1b4654c773611f3
Joel Hutton <Joel.Hutton@arm.com>
2019-08-22 13:37:02 +0100

    pastebin: Update to use https

--- pastebin.sh
+++ pastebin.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-PASTE_TEXT=`cat $1`
-RESPONSE=`curl --user "$USER" --data-urlencode "paste_text=$PASTE_TEXT" http://p.arm.com/\?ns0`
+PASTE_TEXT="$(</dev/stdin)"
+echo -e $PASTE_TEXT
+RESPONSE=`curl --user "$USER" --data-urlencode "paste_text=$PASTE_TEXT" https://p.arm.com/`
 URL=`echo $RESPONSE | grep "Redirected\ to"`
 URL=`echo $URL | sed -e 's/.*\(http.*\)\".*/\1/'`
 echo $URL | xsel -ib
