199ecd18f2820ac113c5408af78cfdc324052fd1
Joel Hutton <Joel.Hutton@arm.com>
2019-08-30 15:28:46 +0100

    tmux-native: unset ssh variables

--- tmux-native.sh
+++ tmux-native.sh
@@ -2,3 +2,6 @@
 tmux set prefix C-b
 tmux set status-bg green
 tmux set status-fg black
+tmux set-environment SSH_CLIENT ""
+tmux set-environment SSH_TTY ""
+tmux set-environment SESSION_TYPE "local"
