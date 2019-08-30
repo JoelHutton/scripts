a9c052b771e1fcd09aa17f13d4b682397ffa4e07
Joel Hutton <Joel.Hutton@arm.com>
2019-08-30 15:26:51 +0100

    share_tmux_session.sh: script to share current tmux session

---
+++ share_tmux_session.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+# Share current ssh session with a guest
+# Dependencies: tmux, xsel, ssh
+#
+# usage: share_tmux_session.sh [command]
+#        commands: share   share current tmux session
+#                  unshare (default) unshare current tmux session
+#
+# You must first create a guest user account then put the following in your
+# /etc/ssh/sshd_config:
+# Match User guest
+#    ForceCommand /tmp/guest_login_command.sh
+# then restart the ssh daemon using:
+# "sudo service ssh restart"
+
+GUEST_USER=guest
+SOCKET=`echo $TMUX | cut -f1 -d','`
+SESSION=`echo $TMUX | cut -f3 -d','`
+SOCKET_DIR=`dirname $SOCKET`
+GUEST_LOGIN_FILE="/tmp/guest_login_command.sh"
+
+if [ -z "$SOCKET" ]
+then
+  echo "can't find tmux socket, are you running this command from within tmux?"
+  exit 1
+elif [ -z "$SESSION" ]
+then
+  echo "can't find tmux session, are you running this command from within tmux?"
+  exit 1
+fi
+
+if [ "$1" == "share" ]
+then
+  echo "sharing current tmux session"
+  chmod 770 "$SOCKET"
+  sudo chgrp "$GUEST_USER" "$SOCKET"
+  sudo chgrp "$GUEST_USER" "$SOCKET_DIR"
+  sudo usermod --expiredate "" "$GUEST_USER"
+  echo "#!/bin/bash" > $GUEST_LOGIN_FILE
+  echo "tmux -S $SOCKET attach -t $SESSION" >> $GUEST_LOGIN_FILE
+  sudo chgrp "$GUEST_USER" "$GUEST_LOGIN_FILE"
+  sudo chmod 770 "$GUEST_LOGIN_FILE"
+  echo "ssh $GUEST_USER@$HOSTNAME" | xsel -ib
+  echo "\"ssh $GUEST_USER@$HOSTNAME\" copied to clipboard"
+else
+  echo "unsharing current tmux session"
+  chmod 700 "$SOCKET"
+  sudo chgrp "$USER" "$SOCKET"
+  sudo chgrp "$USER" "$SOCKET_DIR"
+  sudo rm -f "$GUEST_LOGIN_FILE"
+  echo "disabling guest user"
+  sudo usermod --expiredate 1 "$GUEST_USER"
+  echo "killing guest processes"
+  sudo pkill -u "$GUEST_USER"
+fi
