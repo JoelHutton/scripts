21abe7e68cca54eda11bdb1f57622742abb16662
Joel Hutton <jah1809@gmail.com>
2020-05-04 17:15:12 +0100

    Borg backup exclusions

--- borg_backup.sh
+++ borg_backup.sh
@@ -3,13 +3,20 @@ set -x
 
 DATE=`date "+%Y-%m-%dT%H:%M:%S"`
 
-borg create --verbose --stats --show-rc --list  $REMOTE::$DATE $HOME
+borg create --exclude "$HOME/tmp*"\
+            --exclude "$HOME/Downloads/*"\
+            --exclude "$HOME/.cache/*"\
+            --verbose\
+            --stats\
+            --show-rc\
+            --list\
+            $REMOTE::$DATE $HOME
 
 borg prune                          \
     --list                          \
     --show-rc                       \
-    --keep-within   7d              \
-    --keep-daily    21              \
+    --keep-within   3d              \
+    --keep-daily    7              \
     --keep-weekly   20              \
     --keep-monthly  20              \
     $REMOTE
