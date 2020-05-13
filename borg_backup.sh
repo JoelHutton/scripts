5cdeea537dfdb8debc9b88dfac08810355103d62
Joel Hutton <jah1809@gmail.com>
2020-05-13 10:23:11 +0100

    Add logging

--- borg_backup.sh
+++ borg_backup.sh
@@ -2,15 +2,19 @@
 set -x
 
 DATE=`date "+%Y-%m-%dT%H:%M:%S"`
+LOGDIR=/var/log/borg
+LOGFILE=$LOGDIR/$DATE
 
 borg create --exclude "$HOME/tmp*"\
             --exclude "$HOME/Downloads/*"\
             --exclude "$HOME/.cache/*"\
+            --exclude "$HOME/.config/*"\
+            --exclude "$HOME/pCloudDrive/*"\
             --verbose\
             --stats\
             --show-rc\
             --list\
-            $REMOTE::$DATE $HOME
+            $REMOTE::$DATE $HOME | tee -a $LOGFILE
 
 borg prune                          \
     --list                          \
@@ -19,4 +23,6 @@ borg prune                          \
     --keep-daily    7              \
     --keep-weekly   20              \
     --keep-monthly  20              \
-    $REMOTE
+    $REMOTE | tee -a $LOGFILE
+rm $LOGDIR/latest
+ln -s $LOGDIR/$DATE $LOGDIR/latest
